1bf480ef41a138a150122c0f9ad50bc8dd819649
AbdullahChauhan <acse.uok@gmail.com>
2020-10-21 22:15:26 +0500

    auth service added

---
+++ lib/src/services/local/auth_service.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_starter_app/src/models/user.dart';
+import 'package:observable_ish/observable_ish.dart';
+import 'package:stacked/stacked.dart';
+
+class AuthService with ReactiveServiceMixin {
+  RxValue<User> _user = RxValue<User>();
+  User get user => _user.value;
+
+  AuthService() {
+    listenToReactiveValues([_user]);
+  }
+
+  set user(User user) {
+    _user.value = user;
+  }
+}
