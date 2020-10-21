29c25f01efdea8119d809c8afda3e4dcba33be32
AbdullahChauhan <acse.uok@gmail.com>
2020-10-21 17:36:59 +0500

    stetho package (debug inspector) added

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -3,8 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_starter_app/src/app/app_view.dart';
 import 'package:flutter_starter_app/src/base/utils/constants.dart';
 import 'package:flutter_starter_app/src/configs/app_config.dart';
+import 'package:flutter_stetho/flutter_stetho.dart';
 
 void main() {
+  Stetho.initialize();
   var configuredApp = AppConfig(
     appTitle: Constants.appTitle,
     buildFlavor: "Development",
