d8391547e5de94da76c889cbbbbebbb1c2402fac
AbdullahChauhan <acse.uok@gmail.com>
2021-04-27 20:12:19 +0500

    stetho removed

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -4,10 +4,8 @@ import 'package:flutter_starter_app/src/app/app_view.dart';
 import 'package:flutter_starter_app/src/base/utils/constants.dart';
 import 'package:flutter_starter_app/src/configs/app_config.dart';
 import 'package:flutter_starter_app/src/configs/locator.dart';
-import 'package:flutter_stetho/flutter_stetho.dart';
 
 void main() {
-  Stetho.initialize();
   setupLocator();
   var configuredApp = AppConfig(
     appTitle: Constants.appTitle,
