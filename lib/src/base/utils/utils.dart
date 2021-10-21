724630335e298746452d8b4e2bdba9823087d5f4
Ishaq Hassan <ishaquehassan@gmail.com>
2021-10-21 06:03:40 +0500

    - Renamed textTheme Extesion because of confilict in new textTheme property in BuildContext

--- lib/src/base/utils/utils.dart
+++ lib/src/base/utils/utils.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
 
 extension UIExt on BuildContext {
   double topSpace() => MediaQuery.of(this).padding.top;
   double appBarHeight() => AppBar().preferredSize.height;
   Size screenSize() => MediaQuery.of(this).size;
   ThemeData appTheme() => Theme.of(this);
-  TextTheme textTheme() => Theme.of(this).textTheme;
+  TextTheme appTextTheme() => Theme.of(this).textTheme;
 
   void closeKeyboardIfOpen() {
     FocusScopeNode currentFocus = FocusScope.of(this);
