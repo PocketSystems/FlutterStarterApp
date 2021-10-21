bde1ed9716f8c7c4d874066fea7c67ec96a80edc
Ishaq Hassan <ishaquehassan@gmail.com>
2021-10-21 06:04:22 +0500

    - Added Spacing util widget to ease spacing between widgets

---
+++ lib/src/shared/spacing.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class VerticalSpacing extends SizedBox {
+  const VerticalSpacing([double height = 8]) : super(height: height);
+}
+
+class HorizontalSpacing extends SizedBox {
+  const HorizontalSpacing([double width = 8]) : super(width: width);
+}
